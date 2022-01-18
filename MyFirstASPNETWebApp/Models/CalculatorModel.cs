e505db2077a5719ebcdf9e53591d51fea4241864
haydenridd98 <haydenridd98@gmail.com>
2022-01-18 12:19:46 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/Models/CalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        [Range (0,100)]
+        public int Assignment { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Group { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Exam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Intex { get; set; }
+        
+    }
+}
---
+++ MyFirstASPNETWebApp/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,43 @@
+﻿$("#calculate").click(function () {
+    let sGrade
+    let fFinalGrade
+    fFinalGrade = (parseInt($("#assignment").val()) * .55 + parseInt($("#group").val()) * .05 + parseInt($("#quiz").val()) * .1 + parseInt($("#exam").val()) * .2 + parseInt($("#intex").val()) * .1)
+    if (fFinalGrade >= 94) {
+        sGrade = 'A'
+    }
+    else if (fFinalGrade >= 90) {
+        sGrade = 'A-'
+    }
+    else if (fFinalGrade >= 87) {
+        sGrade = 'B+'
+    }
+    else if (fFinalGrade >= 84) {
+        sGrade = 'B'
+    }
+    else if (fFinalGrade >= 80) {
+        sGrade = 'B-'
+    }
+    else if (fFinalGrade >= 77) {
+        sGrade = 'C+'
+    }
+    else if (fFinalGrade >= 74) {
+        sGrade = 'C'
+    }
+    else if (fFinalGrade >= 70) {
+        sGrade = 'C-'
+    }
+    else if (fFinalGrade >= 67) {
+        sGrade = 'D+'
+    }
+    else if (fFinalGrade >= 64) {
+        sGrade = 'D'
+    }
+    else if (fFinalGrade >= 60) {
+        sGrade = 'D-'
+    }
+    else {
+        sGrade = 'E'
+    }
+    alert('Final Grade: ' + fFinalGrade + '\n' + 'Which is a(n) ' + sGrade)
+})
+
